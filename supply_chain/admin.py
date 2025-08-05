b994cec836cf55fa5c12be44eb5334e9495ef518
Sanusi Akorede <thebeliever002@gmail.com>
2025-08-05 06:37:42 +0100

    Made bunch of configs, might reverse later

--- supply_chain/admin.py
+++ supply_chain/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.apps import apps
+from django.db import models
 
-# Register your models here.
+
+app_models = apps.get_app_config('supply_chain').get_models()
+for model in app_models:
+    admin.site.register(model)
